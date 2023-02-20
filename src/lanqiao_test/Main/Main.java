dcf9bb75d5b3bd92239d42df1755cf926577360b
强风吹拂 <1045547784@qq.com>
2023-02-20 21:07:47 +0800

    code update

---
+++ src/lanqiao_test/Main/Main.java
@@ -0,0 +1,24 @@
+package lanqiao_test.Main;
+
+
+import  java.lang.String;
+
+import java.sql.Array;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Scanner;
+public class Main{
+
+    public static void main(String[] args){
+        Deque<Character> stack = new LinkedList<Character>();
+
+
+    }
+
+
+
+
+}
+
+
